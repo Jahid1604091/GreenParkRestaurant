b1853c19f743c2325aba6cad510e5701e9054e23
Jahid <jh409780@gmail.com>
2021-10-19 17:31:11 +0600

    test

---
+++ js/index.js
@@ -0,0 +1,7 @@
+
+const name = document.getElementById('name').value
+const email = document.getElementById('email').value
+
+document.querySelector('.btn').addEventListener('click',(e)=>{
+    alert("Thanks "+name+ " for your response")
+})
